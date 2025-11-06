0ed71b138da42fcc643e112505784ab74d8f5f1b
Aswun <aswinlutfian32@gmail.com>
2025-11-06 13:42:35 +0700

    membuat spacer

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -5,6 +5,7 @@ package com.example.navigasi.view
 import android.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
@@ -80,6 +81,7 @@ fun FormIsian (
                 label = {Text(text = "Alamat")},
                 onValueChange = {},
             )
+            Spacer(modifier = Modifier.height(30.dp))
 
     }
 }
