9db3e7f9b194f9176a0a162f601b7f00a4b4bd0f
Aswun <aswinlutfian32@gmail.com>
2025-11-06 13:26:27 +0700

    membuat column pada isiRuang

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -3,11 +3,16 @@
 package com.example.navigasi.view
 
 import android.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian (
@@ -20,7 +25,12 @@ fun FormIsian (
                     color = Color.White)},
                 colors = TopAppBarDefaults.
                         TopAppBarColors(colorResource(id = R.color.teal_700))
-            )
+            ) }
+    ) { isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally ) {
 
-        })
+
+    }
 }
