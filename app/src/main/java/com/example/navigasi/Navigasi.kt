bf0f85833c2e2e184de0f0c5d0d32cba9b2dabd2
Aswun <aswinlutfian32@gmail.com>
2025-11-06 13:07:55 +0700

    fix libraries dan dependencies

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,10 +1,14 @@
 package com.example.navigasi
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
 
 enum class Navigasi {
     Formulir,
