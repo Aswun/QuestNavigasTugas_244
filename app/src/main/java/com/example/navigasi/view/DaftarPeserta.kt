a078ab0d69eab075723da8716f432a3a32b27aa9
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:48:43 +0700

    menambahkan scaffold pada daftar peserta

--- app/src/main/java/com/example/navigasi/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/navigasi/view/DaftarPeserta.kt
@@ -3,8 +3,14 @@
 package com.example.navigasi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DaftarPeserta (
@@ -19,4 +25,24 @@ fun DaftarPeserta (
         "ALAMAT" to "Yogyakarta"
     )
 
+    Scaffold(
+        modifier = modifier,
+        containerColor = Color(0xFFF3E5F5), // Latar ungu muda
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "List Daftar Peserta",
+                        color = Color.White,
+                        fontSize = 20.sp
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF9C27B0) // Ungu tua header
+                )
+            )
+        }
+    ) {
+
+    }
 }
