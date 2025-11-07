121601c90845d106514f75cf92217f7f330178bf
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:36:18 +0700

    menambahkan image

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -1,15 +1,23 @@
 package com.example.navigasi.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigasi.R
 
 @Composable
 fun Beranda (
@@ -37,6 +45,13 @@ fun Beranda (
                 color = Color(0xFF6A1B9A), // Ungu tua
                 textAlign = TextAlign.Center,
                 modifier = Modifier.padding(bottom = 30.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(bottom = 20.dp)
             )
         }
     }
