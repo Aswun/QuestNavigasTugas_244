fd6d38c62fb5cde910ac3c25740003d3ee3b533c
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:29:23 +0700

    membuat scaffold pada beranda

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -1,11 +1,18 @@
 package com.example.navigasi.view
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Beranda (
     modifier: Modifier = Modifier,
     onSubmitClick: () -> Unit
 ) {
-
+    // Mengubah containerColor menjadi ungu muda untuk nuansa UI yang berbeda
+    Scaffold(
+        modifier = modifier,
+        containerColor = Color(0xFFE1BEE7)
+    )
 }
