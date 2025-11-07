8ecf9efec707a3433d685ec41938fe5afe4b9c42
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:24:23 +0700

    membuat DataModel.kt

---
+++ app/src/main/java/com/example/navigasi/DataModel.kt
@@ -0,0 +1,8 @@
+package com.example.navigasi
+
+// Data class untuk menampung data formulir
+data class DataDiri(
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = ""
+)
