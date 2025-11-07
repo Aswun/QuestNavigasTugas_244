d0cb97b99463243878505b9815c79075d4868094
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:26:59 +0700

    membuat fun beranda

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -1,2 +1,11 @@
 package com.example.navigasi.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Beranda (
+    modifier: Modifier = Modifier,
+    onSubmitClick: () -> Unit
+) {
+
+}
