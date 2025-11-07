f270dd80d1a6eadddc7f4f45e7bd008a6176fb71
Aswun <aswinlutfian32@gmail.com>
2025-11-07 16:33:58 +0700

    menambahkan text selamat datang pada beranda

--- app/src/main/java/com/example/navigasi/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi/view/Beranda.kt
@@ -1,9 +1,15 @@
 package com.example.navigasi.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Beranda (
@@ -23,6 +29,15 @@ fun Beranda (
                 .padding(20.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
+        ) {
+            Text(
+                text = "Selamat Datang",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF6A1B9A), // Ungu tua
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 30.dp)
+            )
+        }
     }
 }
